b2322771ea4f54c564822254633d0c87abafb5b9
SaintArthurG <cazooklol@gmail.com>
2023-12-01 20:02:21 -0300

    treinando exceptions

---
+++ src/project7/model/entities/Account.java
@@ -0,0 +1,68 @@
+package project7.model.entities;
+
+import project7.model.exceptions.DomainException;
+
+public class Account {
+    private Integer number;
+    private String holder;
+    private Double balance;
+    private Double withdrawLimit;
+
+    public Account() {
+    }
+
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) throws DomainException {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void setWithdrawLimit(Double withdrawLimit) {
+        this.withdrawLimit = withdrawLimit;
+    }
+    public void deposit(Double amount){
+        this.balance += amount;
+    }
+    public void withdraw(Double amount) throws DomainException{
+        if (amount > withdrawLimit){
+            throw new DomainException("The amount exceeds withdraw limit");
+        }
+        if (amount > balance){
+            throw new DomainException("Not enough balance");
+        }
+        this.balance -= amount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ACCOUNT:\nNumber: ").append(number).append("\nHolder: ")
+                .append(holder).append("\nBalance: ").append(balance).append(" \nWithdraw Limit: ").append(withdrawLimit);
+        return sb.toString();
+    }
+}
